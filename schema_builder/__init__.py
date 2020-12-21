4eab46d8ed5e7464a9c2c5a869e7c39a32d67aa8
Michael Cooper <macoop2363@gmail.com>
2020-12-21 12:59:27 -0700

    Add method to get json schema type

--- schema_builder/__init__.py
+++ schema_builder/__init__.py
@@ -1,1 +1,11 @@
-__version__ = '0.1.0'
+def schema_type(value):
+    value_type = type(value)
+    types = {
+        int: 'number',
+        float: 'number',
+        bool: 'boolean',
+        dict: 'object',
+        list: 'array',
+        str: 'string'
+    }
+    return types.get(value_type, 'null')
--- tests/test_schema_builder.py
+++ tests/test_schema_builder.py
@@ -1,5 +1,17 @@
-from schema_builder import __version__
+from schema_builder import *
 
+def test_schema_type():
+    assert schema_type(10) == "number"
+    assert schema_type(10.5) == "number"
 
-def test_version():
-    assert __version__ == '0.1.0'
+    assert schema_type(True) == 'boolean'
+    assert schema_type(False) == 'boolean'
+
+    assert schema_type({ "hello": "world" }) == 'object'
+    assert schema_type({}) == 'object'
+
+    assert schema_type(["hola", "mundo"]) == 'array'
+    assert schema_type([1, 2, 3]) == 'array'
+    assert schema_type([]) == 'array'
+
+    assert schema_type("hello") == 'string'
