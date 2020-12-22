f903e0453928cc2bc505c07cb173a3a9c73e99f9
Jordan <jordan.wallace.williams@gmail.com>
2020-12-21 23:57:14 -0700

    Add clean_data(), remove_opening_and_closing(), remove_new_lines()

--- schema_builder/source_ddl_builder.py
+++ schema_builder/source_ddl_builder.py
@@ -13,10 +13,32 @@ def open_ddl_file(file=None):
 
 
 def parse_table_data():
-  table_data = open_ddl_file()
+  raw_table_data = open_ddl_file()
+  clean_table_data = clean_data(raw_table_data)
   table_dict = {}
 
   return table_dict
+
+
+def clean_data(data):
+  step1 = remove_opening_and_closing(data)
+  step2 = remove_new_lines(step1)
+
+  return step2
+
+
+def remove_opening_and_closing(data):
+  clean_data = data[4:-1]
+
+  return clean_data
+
+
+def remove_new_lines(data):
+  for line in data:
+    new_line = line.split('(\n')
+    new_line = line.split(',\n')
+    new_line = line.split('\t')
+    test = 'stop'
 
 
 my_table = parse_table_data()
