1bfc674287e035016570a8b4cc8f0bf76bd47c13
Jordan <jordan.wallace.williams@gmail.com>
2020-12-22 00:26:53 -0700

    Add remove_new_lines() and create_table_dict(). Update remove_unnecessary_items(), clean_data(), and parse_table_data()

--- schema_builder/source_ddl_builder.py
+++ schema_builder/source_ddl_builder.py
@@ -15,30 +15,41 @@ def open_ddl_file(file=None):
 def parse_table_data():
   raw_table_data = open_ddl_file()
   clean_table_data = clean_data(raw_table_data)
-  table_dict = {}
+  table_dict = create_table_dict(clean_table_data)
 
   return table_dict
 
 
 def clean_data(data):
-  step1 = remove_opening_and_closing(data)
+  step1 = remove_unncessary_items(data)
   step2 = remove_new_lines(step1)
 
   return step2
 
 
-def remove_opening_and_closing(data):
+def remove_unncessary_items(data):
   clean_data = data[4:-1]
 
   return clean_data
 
 
 def remove_new_lines(data):
+  new_line_list = []
+
   for line in data:
-    new_line = line.split('(\n')
-    new_line = line.split(',\n')
-    new_line = line.split('\t')
-    test = 'stop'
+    if line == data[0]:
+      new_line_list.append(line[:-3])
+    else:
+      new_line_list.append(line[1:-2].split(' '))
+    
+
+    return new_line_list
+
+
+def create_table_dict(data):
+  table_dict = {}
+
+  return table_dict
 
 
 my_table = parse_table_data()
