a247d3c079e656329968bde5cd7df92d15c0f125
Jordan <jordan.wallace.williams@gmail.com>
2021-01-06 00:58:10 -0700

    Add test framework for each function in schema_builder to be tested

--- tests/test_schema_builder.py
+++ tests/test_schema_builder.py
@@ -7,4 +7,40 @@ import pytest_cov
 
 
 def test_version():
-  assert __version__ == '0.2.0'
+    assert __version__ == '0.2.0'
+
+
+@pytest.mark.skip(reason="Test needs to be created.")
+def test_find_data_type():
+    pass
+
+
+@pytest.mark.skip(reason="Test needs to be created.")
+def test_create_table_dict():
+    pass
+
+
+@pytest.mark.skip(reason="Test needs to be created.")
+def test_create_json_schema_dict():
+    pass
+
+
+@pytest.mark.skip(reason="Test needs to be created.")
+def test_create_json_schema_file():
+    pass
+
+
+@pytest.mark.skip(reason="Test needs to be created.")
+def test_schema_from_ddl():
+    pass
+
+
+@pytest.mark.skip(reason="Test needs to be created.")
+def test_schema_from_table():
+    pass
+
+
+@pytest.mark.skip(reason="Test needs to be created.")
+def test_build_json_schema():
+    pass
+
