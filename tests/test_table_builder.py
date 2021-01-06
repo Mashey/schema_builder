c10e6f286c6cd75e12dd018bfc3caf69b8c946e5
Jordan <jordan.wallace.williams@gmail.com>
2021-01-06 00:56:44 -0700

    Add test file for the builder_table_list.py module

---
+++ tests/test_table_builder.py
@@ -0,0 +1,13 @@
+from schema_builder.builder_table_list import *
+import pytest
+import pytest_cov
+
+
+@pytest.mark.skip(reason="Test needs to be created.")
+def test_parse_formatted_table():
+    # The data format for this test needs to be stubbed
+    # In the carelogic_service run parse_table_schema() to get the needed data
+
+    # clean_data = parse_formatted_table(table)
+    pass
+
