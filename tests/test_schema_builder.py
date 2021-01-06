090879a5f526a9f605bb5ef864309af9514e7bbd
Jordan <jordan.wallace.williams@gmail.com>
2021-01-06 00:27:47 -0700

    Add file to test the schema_builder package

---
+++ tests/test_schema_builder.py
@@ -0,0 +1,10 @@
+from schema_builder import __version__
+from schema_builder import *
+from schema_builder.builder_ddl import *
+from schema_builder.builder_table_list import *
+import pytest
+import pytest_cov
+
+
+def test_one():
+  pass
