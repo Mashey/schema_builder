653aa7211da19ed4d9f73ded5905f0a90bf54b4b
Jordan <jordan.wallace.williams@gmail.com>
2021-01-17 17:52:15 -0700

    Update parse_formatted_table() to accept 'table_name' parameter, and the return value structure now matches that of clean_data()

--- schema_builder/builder_table_list.py
+++ schema_builder/builder_table_list.py
@@ -1,9 +1,9 @@
-def parse_formatted_table(table):
-    end_data_index = table.index(('', None, None))
-    clean_table = table[1:end_data_index]
-    parsed_table = []
+def parse_formatted_table(table_data, table_name):
+    end_data_index = table_data.index(('', None, None))
+    clean_table = table_data[1:end_data_index]
+    parsed_table = [table_name, []]
 
     for row in clean_table:
-        parsed_table.append([row[0], row[1]])
+        parsed_table[1].append([row[0], row[1]])
 
     return parsed_table
